dc90cd1afbc9babdaf977a9b5b36ed0fab878587
jorgegm2210 <65723723+jorgegm2210@users.noreply.github.com>
2020-05-31 11:57:07 +0200

    Add files via upload

--- practicaGit/src/practicaGit/Ventana.java
+++ practicaGit/src/practicaGit/Ventana.java
@@ -30,12 +30,15 @@ public class Ventana extends javax.swing.JFrame {
         lblMes = new javax.swing.JLabel();
         cmbMes = new javax.swing.JComboBox<>();
         lblMensaje = new javax.swing.JLabel();
+        btnVerDias = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
         lblMes.setText("Selecciona un mes : ");
 
         lblMensaje.setText("JLabel2");
+
+        btnVerDias.setText("Ver Dias");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -44,6 +47,7 @@ public class Ventana extends javax.swing.JFrame {
             .addGroup(layout.createSequentialGroup()
                 .addGap(28, 28, 28)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(btnVerDias)
                     .addComponent(lblMes)
                     .addComponent(cmbMes, javax.swing.GroupLayout.PREFERRED_SIZE, 149, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(lblMensaje, javax.swing.GroupLayout.PREFERRED_SIZE, 319, javax.swing.GroupLayout.PREFERRED_SIZE))
@@ -58,7 +62,9 @@ public class Ventana extends javax.swing.JFrame {
                 .addComponent(cmbMes, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(46, 46, 46)
                 .addComponent(lblMensaje, javax.swing.GroupLayout.PREFERRED_SIZE, 37, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(131, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(btnVerDias)
+                .addContainerGap(87, Short.MAX_VALUE))
         );
 
         pack();
@@ -100,6 +106,7 @@ public class Ventana extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnVerDias;
     private javax.swing.JComboBox<String> cmbMes;
     private javax.swing.JLabel lblMensaje;
     private javax.swing.JLabel lblMes;
